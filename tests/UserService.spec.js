f58cb98a9d9c24d5c59058b2c30ca84c1966d5c3
JENNIFER YAMADA <jenny.yamada@gmail.com>
2016-10-03 11:51:34 -0700

    done

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,30 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
 
+  var UserService, $httpBackend;
+
+  beforeEach(inject(function($injector){
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond({first_name: 'Dolly', email: 'dolly@dollywood.com'});
+  }));
+
+  it('should get the users information', function(done){
+    $httpBackend.expectGET('/rest/user');
+
+    UserService
+     .getUser()
+     .then(function(res){
+       var data = res.data;
+       if(data.email === 'dolly@dollywood.com' && data.first_name == 'Dolly'){
+         done();
+       }
+     });
+      $httpBackend.flush();
+  });
+
+  it('should join first and last name', function(){
+    expect(UserService.createFullName({first_name: 'First', last_name: 'Last'})).toEqual('First Last');
+  })
 });
